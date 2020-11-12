9df4da1fbd0a06f35255293918d5ec0273ec8cb1
Bernard <bh20@hw.ac.uk>
2020-11-12 13:26:36 +0000

    made linear classifier on train and test set

--- Neural Networks/linear_classifier.py
+++ Neural Networks/linear_classifier.py
@@ -1,8 +1,10 @@
-from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import confusion_matrix
 from sklearn.svm import LinearSVC
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from imutils  import paths
+from sklearn.model_selection import cross_val_score
+
+
 import numpy as np
 import pandas as pd
 import argparse
@@ -20,11 +22,8 @@ y_data = pd.read_csv(y_train_smpl)
 data_array = x_data.values
 labels_array = y_data.values 
 
-print(data_array.size)
-print(labels_array.size)
-
 (trainData, testData, trainLabels, testLabels) = train_test_split(
-    data_array, labels_array, test_size = 0.25, random_state=42
+    data_array, labels_array, test_size = 0.15, random_state=42
 ) #75% for training 25% for testing
 
 
@@ -44,14 +43,22 @@ def extract_color_hostogram(image, bins=(8,8,8)):
 target_names = ['20','30','50','60','70','left turn', 'right turn',
                 'predestrian crossing', 'children', 'cycle route ahead']
 
+
 #train the linear classification
-print("[INFO] evaluating classifier")
+print("[INFO] training Linear SVM classifier...")
 model = LinearSVC()
-model.fit(trainData, trainLabels)
+model.fit(trainData, trainLabels.ravel())
 
 #evaluate the classifier
 print("[INFO] evaluating classifier...")
 predications = model.predict(testData)
-print(classification_report(testLabels, predications, 
+print(classification_report(testLabels.ravel(), predications, 
     target_names=target_names))
 
+label_numbers = [0,1,2,3,4,5,6,7,8,9]
+
+print("confusion_matrix")
+print(confusion_matrix(testLabels, predications, labels = label_numbers))
+
+#scores = cross_val_score(model, data_array, labels_array.ravel(), cv = 10)
+#print("Accuracy: %0.f (+/- %0.2f)" % (scores.mean(), scores.std()*2))
