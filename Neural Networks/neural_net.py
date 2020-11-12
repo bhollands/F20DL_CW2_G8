0ecc274d875f694d0620a29cd7dc374771e7ab4e
Bernard <bh20@hw.ac.uk>
2020-11-12 12:11:51 +0000

    did linear classifer

--- Neural Networks/neural_net.py
+++ Neural Networks/neural_net.py
@@ -1,1 +1,57 @@
-print("Hello")
+from sklearn.preprocessing import LabelEncoder
+from sklearn.svm import LinearSVC
+from sklearn.metrics import classification_report
+from sklearn.model_selection import train_test_split
+from imutils  import paths
+import numpy as np
+import pandas as pd
+import argparse
+import imutils
+import cv2
+import os 
+
+
+x_train_smpl = "x_train_gr_smpl_reduced.csv"
+y_train_smpl = "y_train_smpl.csv"
+
+x_data = pd.read_csv(x_train_smpl)
+y_data = pd.read_csv(y_train_smpl)
+
+data_array = x_data.values
+labels_array = y_data.values 
+
+print(data_array.size)
+print(labels_array.size)
+
+(trainData, testData, trainLabels, testLabels) = train_test_split(
+    data_array, labels_array, test_size = 0.25, random_state=42
+) #75% for training 25% for testing
+
+
+
+def extract_color_hostogram(image, bins=(8,8,8)):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([hsv], [0,1,2], None, bins, [0, 180, 0, 256, 0, 256])
+    if imutils.is_cv2():
+        hist = cv2.normalize(hist)
+
+    else:
+        cv2.normalize(hist, hist)
+    
+    return hist.flatten()
+
+
+target_names = ['20','30','50','60','70','left turn', 'right turn',
+                'predestrian crossing', 'children', 'cycle route ahead']
+
+#train the linear classification
+print("[INFO] evaluating classifier")
+model = LinearSVC()
+model.fit(trainData, trainLabels)
+
+#evaluate the classifier
+print("[INFO] evaluating classifier...")
+predications = model.predict(testData)
+print(classification_report(testLabels, predications, 
+    target_names=target_names))
+
